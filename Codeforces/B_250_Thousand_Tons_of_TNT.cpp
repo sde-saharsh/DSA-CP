e4d499d264a6682daa09f8a65a313253b9dddc6b
saharsh <saharshkhalokar14@gmail.com>
2026-01-05 10:50:14 +0530

    codeforces-template-added

---
+++ Codeforces/B_250_Thousand_Tons_of_TNT.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define vi vector<int> 
+#define vll vector<ll>
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define per(i, a, b) for (int i = (a); i >= (b); --i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)((x).size())
+
+
+void solve() {
+    int n,k;
+    cin>>n>>k;
+
+    vll a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+
+    
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
