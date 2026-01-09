c034b208839e987d5355a41ea797b3153e4ac0be
saharsh <saharshkhalokar14@gmail.com>
2026-01-10 00:10:07 +0530

    10-jan

---
+++ Codeforces/A_Sum_of_Round_Numbers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define vi vector<int> 
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+
+void reverse(vector<long long>& a){
+    int n = a.size();
+    for(int i=0;i<n/2;i++){
+        int temp = a[i];
+        a[i] = a[n-i-1];
+        a[n-i-1] = temp;
+    }
+}
+
+void solve() {
+    ll n;
+    cin>>n;
+
+    vll a;
+    ll x = n;
+    while(x>0){
+        a.push_back(x%10);
+        x = x/10;
+    }
+    reverse(a);
+    
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while (t--) solve();
+}
