690f2666aefa4cc77aa16f9fc070117fd5bcaa97
saharsh <saharshkhalokar14@gmail.com>
2026-01-09 11:18:54 +0530

    9-jan

---
+++ Codeforces/A_Bit.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define vi vector<int> 
+#define vll vector<ll>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+
+
+void solve() {
+    //code 
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    int x = 0;
+    while (t--){
+        string s;
+        cin>>s;
+        for(int i=0;i<=2;i++){
+            if(s[i]=='+'){
+                x++;
+                break;
+            }
+            if(s[i]=='-'){
+                x--;
+                break;
+            }
+        }
+    }
+    cout<<x<<endl;
+    return 0;
+}
