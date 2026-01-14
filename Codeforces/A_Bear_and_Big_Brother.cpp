c1e6835647abdf4fc3e6ac257a1b92785ff5363a
saharsh <saharshkhalokar14@gmail.com>
2026-01-14 16:51:55 +0530

    14-jan

---
+++ Codeforces/A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define vi vector<int> 
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+
+
+void solve() {
+    ll a ,b;
+    cin>>a>>b;
+
+    int count = 0;
+    while(a<=b){
+        a = a*3;
+        b = b*2;
+        count++;
+    }
+    cout<<count<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    // cin >> t;
+    while (t--) solve();
+}
